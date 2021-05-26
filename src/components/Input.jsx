5e5d9eed43fde3d1dc01f9b950b447a31461efd9
madruga665 <madruga665@hotmail.com>
2021-05-26 13:40:31 -0300

    renderiza o componente Card dentro da Home

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,44 +1,15 @@
 import React, { Component } from "react";
-import Card from "./Card";
 export default class Input extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cep: "",
-      data: {},
-    };
-  }
-
-  fetchCep = async (inputCep) => {
-    const response = await fetch(`https://viacep.com.br/ws/${inputCep}/json/`);
-    const data = await response.json();
-    this.setState({ data: data });
-    console.log(data);
-  };
-
-  handleChange = (event) => {
-    this.setState({
-      cep: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    const { cep } = this.state;
-    event.preventDefault();
-    this.fetchCep(cep);
-  };
-
   render() {
-    const { cep } = this.state;
+    const { handleChange, handleSubmit } = this.props;
     return (
       <>
         <form>
-          <input type="text" value={cep} onChange={this.handleChange} />
-          <button type="submit" onClick={this.handleSubmit}>
+          <input type="text" onChange={(event) => handleChange(event)} />
+          <button type="submit" onClick={(event) => handleSubmit(event)}>
             Buscar
           </button>
         </form>
-        <Card data={this.state} />
       </>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
+import Card from "../components/Card";
 import Header from "../components/Header";
 import Input from "../components/Input";
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cep: "",
+      data: {},
+    };
+  }
+
+  fetchCep = async (inputCep) => {
+    const response = await fetch(`https://viacep.com.br/ws/${inputCep}/json/`);
+    const data = await response.json();
+    this.setState({ data: data });
+    console.log(data);
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      cep: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    const { cep } = this.state;
+    event.preventDefault();
+    this.fetchCep(cep);
+  };
+
   render() {
     return (
       <main className="home">
         <Header />
-        <Input />
+        <Input
+          handleChange={(value) => this.handleChange(value)}
+          handleSubmit={this.handleSubmit}
+        />
+        <Card data={this.state} />
       </main>
     );
   }
