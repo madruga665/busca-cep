5f8eb076a22497b8647869ea30df5939904d5ed7
madruga665 <madruga665@hotmail.com>
2021-06-03 02:33:51 -0300

    desinstala o tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div className="bg-yellow-300 w-100 h-20 flex flex-1 justify-center items-center">
-        <h1 className="text-3xl">Busca Cep</h1>
+      <div>
+        <h1>Busca Cep</h1>
       </div>
     )
   }
