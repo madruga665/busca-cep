0e4480e4db759b15b4c8660d1b3a75b0e2504a9f
madruga665 <madruga665@hotmail.com>
2021-06-05 08:35:41 -0300

    add position fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer className="absolute bottom-0 bg-yellow-300 h-10 w-full 
+      <footer className="fixed mt-8 bottom-0 bg-yellow-300 h-10 w-full 
       flex justify-center items-center p-6 text-center">
         <span>💚️Criado com muito amor e dedicação &copy;Madruga 2021💚️</span>
       </footer>
