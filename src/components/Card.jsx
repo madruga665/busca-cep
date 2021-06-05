74f806984275e241d6947a5a4f919c5ed50137d7
madruga665 <madruga665@hotmail.com>
2021-06-05 06:06:19 -0300

    add cor para destacar o resultado da busca

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./styles/Card.css";
 
 export default class Card extends Component {
   result = () => {
@@ -10,22 +9,40 @@ export default class Card extends Component {
     return (
       <>
         <div className="flex flex-col relative w-full">
-          <div className="m-1 mb-3 text-gray-600 flex w-full">
+          <div
+            className="m-1 mb-3 text-gray-600 flex w-full 
+          bg-yellow-100 border rounded p-1 border-white"
+          >
             Cep:<p className="absolute right-0">{cep}</p>
           </div>
-          <div className="m-1 mb-3 text-gray-600 flex w-full">
+          <div
+            className="m-1 mb-3 text-gray-600 flex w-full 
+          bg-yellow-100 border rounded p-1 border-white"
+          >
             Logradouro:<p className="absolute right-0">{logradouro}</p>
           </div>
-          <div className="m-1 mb-3 text-gray-600 flex w-full">
+          <div
+            className="m-1 mb-3 text-gray-600 flex w-full 
+          bg-yellow-100 border rounded p-1 border-white"
+          >
             Bairro:<p className="absolute right-0">{bairro}</p>
           </div>
-          <div className="m-1 mb-3 text-gray-600 flex w-full">
+          <div
+            className="m-1 mb-3 text-gray-600 flex w-full 
+          bg-yellow-100 border rounded p-1 border-white"
+          >
             Cidade:<p className="absolute right-0">{localidade}</p>
           </div>
-          <div className="m-1 mb-3 text-gray-600 flex w-full">
+          <div
+            className="m-1 mb-3 text-gray-600 flex w-full 
+          bg-yellow-100 border rounded p-1 border-white"
+          >
             Estado:<p className="absolute right-0">{uf}</p>
           </div>
-          <div className="m-1 mb-3 text-gray-600 flex w-full">
+          <div
+            className="m-1 mb-3 text-gray-600 flex w-full 
+          bg-yellow-100 border rounded p-1 border-white"
+          >
             Complemento:<p className="absolute right-0">{complemento}</p>
           </div>
         </div>
@@ -36,7 +53,8 @@ export default class Card extends Component {
     const { data } = this.props;
 
     return (
-      <section className="bg-yellow-200 w-full sm:w-11/12 md:w-4/5 lg:w-2/5 xl:w-2/5 flex flex-col items-center py-6 px-3 my-4 shadow-xl border-t-8 border-yellow-400 rounded-lg">
+      <section className="bg-yellow-200 w-full sm:w-11/12 md:w-4/5 lg:w-2/5 xl:w-2/5 
+      flex flex-col items-center py-6 px-3 my-4 shadow-xl border-t-8 border-yellow-400 rounded-lg">
         <h2 className="text-xl font-semibold mb-10 border-b-2 border-gray-600">
           Resultado da busca
         </h2>
